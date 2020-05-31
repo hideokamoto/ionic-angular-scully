928dcd20a94a4cbad78e4219851fa5f9cf411b07
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-31 18:47:29 +0900

    generate

---
+++ scully.app.config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    projectRoot: "./src",
+    projectName: "app",
+    outDir: './dist/static',
+    routes: {}
+};
