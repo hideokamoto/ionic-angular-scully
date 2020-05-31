a0c1ee3a3f61dc704e942478baf24725856849b4
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-31 18:39:30 +0900

    add scully

---
+++ scully.app.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "app",
+  outDir: './dist/static',
+  routes: {
+  }
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ScullyLibModule],
   providers: [
     StatusBar,
     SplashScreen,
